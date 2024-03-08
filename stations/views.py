05e20aac52853ea26930f815f69aed214404b068
AlexTruee <133477855+AlexTruee@users.noreply.github.com>
2024-03-08 12:43:47 +0500

    First commit

---
+++ stations/views.py
@@ -0,0 +1,32 @@
+import csv
+
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from pagination import settings
+
+
+def index(request):
+    return redirect(reverse('bus_stations'))
+
+
+def bus_stations(request):
+    # получите текущую страницу и передайте ее в контекст
+    # также передайте в контекст список станций на странице
+
+    # context = {
+    #     'bus_stations': ...,
+    #     'page': ...,
+    # }
+    page_number = int(request.GET.get('page', 1))
+    csv_file = settings.BUS_STATION_CSV
+    with open(csv_file, newline='', encoding='UTF-8') as f:
+        bus_stations = list(csv.DictReader(f))
+        paginator = Paginator(bus_stations, 10)
+        page_obj = paginator.get_page(page_number)
+        context = {
+            'bus_stations': page_obj.object_list,
+            'page': page_obj,
+        }
+    return render(request, 'stations/index.html', context)
